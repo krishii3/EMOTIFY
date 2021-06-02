ad781eabfde95777fb1df209849f89469a23c396
krishii3 <mridulkrishn3@gmail.com>
2021-06-02 14:46:10 +0530

    changes

--- static/JS/app.js
+++ static/JS/app.js
@@ -1,3 +1,68 @@
+const btnANGRY = document.querySelector('#btnANGRY')
+const btnSURPRISED = document.querySelector('#btnSURPRISED')
+const btnDISGUSTED = document.querySelector('#btnDISGUSTED')
+const btnNEUTRAL = document.querySelector('#btnNEUTRAL')
+const btnSAD = document.querySelector('#btnSAD')
+const btnHAPPY = document.querySelector('#btnHAPPY')
+const btnFEARFUL = document.querySelector('#btnFEARFUL')
+
+
+const fetchAttribute = ()=> {
+    
+    let attrib = ''
+    fetch('/getRandomImage/attribute', {
+        method: 'GET'
+    })
+    .then(res=> res.json())
+    .then(res => {
+        if(!res.attrib){
+            attrib=null;
+        }
+        attrib=(res.attrib)
+    })
+
+    return attrib;
+}
+
+const checkInput = (attrib)=>{
+    
+    if(fetchAttribute()===null){
+        console.log("KUCH TO GADBAD HAI");
+    }
+    else
+    {
+        if(attrib===fetchAttribute()){
+            console.log("jalebi baby")
+        }
+        else{
+            console.log("no jalebi baby")
+        }
+    }
+}
+
+btnANGRY.addEventListener('click',()=>{checkInput('angry')
+    
+})
+btnSURPRISED.addEventListener('click',()=>{checkInput('surprised')
+    
+})
+btnDISGUSTED.addEventListener('click',()=>{checkInput('disgusted')
+    
+})
+btnNEUTRAL.addEventListener('click',()=>{checkInput('neutral')
+    
+})
+btnSAD.addEventListener('click',()=>{checkInput('sad')
+    
+})
+btnHAPPY.addEventListener('click',()=>{checkInput('happy')
+    
+})
+btnFEARFUL.addEventListener('click',()=>{checkInput('fearful')
+    
+})
+
+
 const img = document.querySelector('#randomImage')
 
 const next = document.querySelector('#next')
@@ -11,3 +76,4 @@ next.addEventListener('click',()=>{
         console.log(res.attrib)
     })
 })
+
