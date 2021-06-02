3ff9df36cf72d7cbb2cdf5aaa4959b2cfe4222fd
bhanu8047 <bpsbhanupratap96@gmail.com>
2021-06-02 16:08:04 +0530

    added points for answers.

--- static/JS/app.js
+++ static/JS/app.js
@@ -7,6 +7,137 @@ const btnHAPPY = document.querySelector('#btnHAPPY')
 const btnFEARFUL = document.querySelector('#btnFEARFUL')
 let counter = 0
 
+// HELPER FUNCTIONSS
+
+const addUserTimeTaken = (usedTime) => {
+    let userTime = []
+    let savedTimes = JSON.parse(localStorage.getItem("userTime"))
+    console.log(savedTimes)
+    userTime = [...savedTimes]
+    userTime.push(usedTime)
+    localStorage.setItem("userTime", JSON.stringify(userTime))
+}
+const stopTimer = () => {
+    countDown.stop()
+    countViz.stop()
+    disableInput()
+}
+
+const disableInput = () => {
+    btnANGRY.setAttribute("disabled",true)
+    btnSURPRISED.setAttribute("disabled",true)
+    btnDISGUSTED.setAttribute("disabled",true)
+    btnNEUTRAL.setAttribute("disabled",true)
+    btnSAD.setAttribute("disabled",true)
+    btnFEARFUL.setAttribute("disabled",true)
+    btnHAPPY.setAttribute("disabled",true)
+}
+const enableInput = () => {
+    btnANGRY.removeAttribute("disabled")
+    btnSURPRISED.removeAttribute("disabled")
+    btnDISGUSTED.removeAttribute("disabled")
+    btnNEUTRAL.removeAttribute("disabled")
+    btnSAD.removeAttribute("disabled")
+    btnFEARFUL.removeAttribute("disabled")
+    btnHAPPY.removeAttribute("disabled")
+}
+
+// FUNCTION FOR TIMERS 
+// THIS IS FOR TEXT TIMER
+function Countdown(elem, seconds) {
+    var that = {};
+  
+    that.elem = elem;
+    that.seconds = seconds;
+    that.totalTime = seconds * 100;
+    that.usedTime = 0;
+    that.startTime = +new Date();
+    that.timer = null;
+  
+    that.count = function() {
+      that.usedTime = Math.floor((+new Date() - that.startTime) / 10);
+  
+      var tt = that.totalTime - that.usedTime;
+      if (tt <= 0) {
+        that.elem.innerHTML = '00.00';
+        clearInterval(that.timer);
+      } else {
+        var mi = Math.floor(tt / (60 * 100));
+        var ss = Math.floor((tt - mi * 60 * 100) / 100);
+        var ms = tt - Math.floor(tt / 100) * 100;
+  
+        that.elem.innerHTML =  that.fillZero(ss) + "." + that.fillZero(ms);
+      }
+    };
+    
+    that.init = function() {
+      if(that.timer){
+        clearInterval(that.timer);
+        that.elem.innerHTML = '00.00';
+        that.totalTime = seconds * 100;
+        that.usedTime = 0;
+        that.startTime = +new Date();
+        that.timer = null;
+      }
+    };
+  
+    that.start = function() {
+      if(!that.timer){
+         that.timer = setInterval(that.count, 1);
+      }
+    };
+  
+    that.stop = function() {
+    //   console.log('usedTime = ' + countDown.usedTime);
+      addUserTimeTaken(countDown.usedTime)
+      if (that.timer) clearInterval(that.timer);
+    };
+  
+    that.fillZero = function(num) {
+      return num < 10 ? '0' + num : num;
+    };
+  
+    return that;
+  }
+//   THIS IS FOR VISUALISATION
+  function timerCountDown (){
+
+    const timer = document.getElementById("timerBar")
+    var that = {}
+    that.timer = null
+    that.width = 500
+
+    that.init = function(){
+        if(that.timer) {
+            clearInterval(that.timer)
+            timer.style.width = 500
+        }
+    }
+    that.frame = function(){
+        if(that.width <= 0){
+            clearInterval(that.timer)
+            disableInput()
+        } else {
+            --that.width
+            timer.style.width = that.width + 'px'
+        }
+    }  
+    that.start = function() {
+        if(!that.timer) {
+            that.timer = setInterval(that.frame, 10)
+        }
+    }
+    that.stop = function(){
+        if(that.timer){
+            clearInterval(that.timer)
+        }
+    }
+    return that
+}
+
+
+
+
 const updateCounter = () => {
     const counterBtn = document.querySelector('#counter')
     counterBtn.textContent = counter
@@ -18,6 +149,7 @@ const fetchAttribute = async ()=> {
     }).then(res => res.json())
 }
 const checkInput = async (attrib)=>{
+    stopTimer()
     const data = await fetchAttribute()
     if(!data.attrib) {
         let message = "something went wrong, please restart your game."
@@ -44,7 +176,10 @@ const getRandomImage = async () => {
 
 const photobox = document.querySelector('#photobox')
 const next = document.querySelector('#next')
+const time = document.querySelector('#time')
+let countDown, countViz
 next.addEventListener('click', async()=>{
+    enableInput()
     photobox.textContent = ''
     const img = new Image()
     const dataUrl = await getRandomImage()
@@ -53,5 +188,8 @@ next.addEventListener('click', async()=>{
     img.width = '200'
     img.height = '200'
     photobox.appendChild(img)
+    countDown = new Countdown(time, 5)
+    countViz = new timerCountDown()
+    countDown.start()
+    countViz.start()
 })
-
