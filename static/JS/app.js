aea2410cd3d3ccb9efe64f391cf3954fccf9e8b9
bhanu8047 <bpsbhanupratap96@gmail.com>
2021-06-02 15:20:19 +0530

    added points for answers.

--- static/JS/app.js
+++ static/JS/app.js
@@ -5,75 +5,42 @@ const btnNEUTRAL = document.querySelector('#btnNEUTRAL')
 const btnSAD = document.querySelector('#btnSAD')
 const btnHAPPY = document.querySelector('#btnHAPPY')
 const btnFEARFUL = document.querySelector('#btnFEARFUL')
+let counter = 0
 
+const updateCounter = () => {
+    const counterBtn = document.querySelector('#counter')
+    counterBtn.textContent = counter
+}
 
-const fetchAttribute = ()=> {
-    
-    let attrib = ''
-    fetch('/getRandomImage/attribute', {
+const fetchAttribute = async ()=> {
+    return await fetch('/getRandomImage/attribute', {
         method: 'GET'
-    })
-    .then(res=> res.json())
-    .then(res => {
-        if(!res.attrib){
-            attrib=null;
-        }
-        attrib=(res.attrib)
-    })
-
-    return attrib;
+    }).then(res => res.json())
+}
+const checkInput = async (attrib)=>{
+    const data = await fetchAttribute()
+    if(!data.attrib) {
+        let message = "something went wrong, please restart your game."
+        console.log(message);
+    } else {
+        attrib === data.attrib ? counter+=1 : ''
+    }
+    updateCounter()
 }
 
-const checkInput = (attrib)=>{
-    
-    if(fetchAttribute()===null){
-        console.log("KUCH TO GADBAD HAI");
-    }
-    else
-    {
-        if(attrib===fetchAttribute()){
-            console.log("jalebi baby")
-        }
-        else{
-            console.log("no jalebi baby")
-        }
-    }
-}
-
-btnANGRY.addEventListener('click',()=>{checkInput('angry')
-    
-})
-btnSURPRISED.addEventListener('click',()=>{checkInput('surprised')
-    
-})
-btnDISGUSTED.addEventListener('click',()=>{checkInput('disgusted')
-    
-})
-btnNEUTRAL.addEventListener('click',()=>{checkInput('neutral')
-    
-})
-btnSAD.addEventListener('click',()=>{checkInput('sad')
-    
-})
-btnHAPPY.addEventListener('click',()=>{checkInput('happy')
-    
-})
-btnFEARFUL.addEventListener('click',()=>{checkInput('fearful')
-    
-})
+btnANGRY.addEventListener('click', async()=>checkInput('angry'))
+btnSURPRISED.addEventListener('click',()=>checkInput('surprised'))
+btnDISGUSTED.addEventListener('click',()=>checkInput('disgusted'))
+btnNEUTRAL.addEventListener('click',()=>checkInput('neutral'))
+btnSAD.addEventListener('click',()=>checkInput('sad'))
+btnHAPPY.addEventListener('click',()=>checkInput('happy'))
+btnFEARFUL.addEventListener('click',()=>checkInput('fearful'))
 
 
 const img = document.querySelector('#randomImage')
 
 const next = document.querySelector('#next')
 next.addEventListener('click',()=>{
-
-    fetch('/getRandomImage/attribute', {
-        method: 'GET'
-    })
-    .then(res=> res.json())
-    .then(res => {
-        console.log(res.attrib)
-    })
+    localStorage.clear()
 })
 
