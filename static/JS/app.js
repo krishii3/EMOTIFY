07aa65292909fceae7ddf3a370723da12ce5f128
bhanu8047 <bpsbhanupratap96@gmail.com>
2021-06-07 03:08:07 +0530

    responsiveness added

--- static/JS/app.js
+++ static/JS/app.js
@@ -5,6 +5,8 @@ const btnNEUTRAL = document.querySelector('#btnNEUTRAL')
 const btnSAD = document.querySelector('#btnSAD')
 const btnHAPPY = document.querySelector('#btnHAPPY')
 const btnFEARFUL = document.querySelector('#btnFEARFUL')
+const btnNext=document.querySelector('#next')
+const btnEnd=document.querySelector('#end')
 let counter = 0
 localStorage.setItem("userTime", JSON.stringify([]))
 // HELPER FUNCTIONSS
@@ -28,7 +30,10 @@ const disableInput = () => {
     btnSAD.setAttribute("disabled",true)
     btnFEARFUL.setAttribute("disabled",true)
     btnHAPPY.setAttribute("disabled",true)
+    // btnNext.setAttribute("disabled",true)
+    // btnEnd.setAttribute("disabled",true)
 }
+
 const enableInput = () => {
     btnANGRY.removeAttribute("disabled")
     btnSURPRISED.removeAttribute("disabled")
@@ -37,6 +42,8 @@ const enableInput = () => {
     btnSAD.removeAttribute("disabled")
     btnFEARFUL.removeAttribute("disabled")
     btnHAPPY.removeAttribute("disabled")
+    // btnNext.removeAttribute("disabled")
+    // btnEnd.removeAttribute("disabled")
 }
 
 // FUNCTION FOR TIMERS 
@@ -200,3 +207,12 @@ btnNEUTRAL.addEventListener('click',()=>checkInput('neutral'))
 btnSAD.addEventListener('click',()=>checkInput('sad'))
 btnHAPPY.addEventListener('click',()=>checkInput('happy'))
 btnFEARFUL.addEventListener('click',()=>checkInput('fearful'))
+
+
+// IMAGES WIDTH HANDLERS
+
+const emojis = document.querySelectorAll('img')
+emojis.forEach(img => {
+    img.width = 20
+    img.height = 20
+})
